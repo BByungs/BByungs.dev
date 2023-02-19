42c748aec68e027f2402148389939d4aa76d915d
안병진 <byungjin0120@gmail.com>
2023-02-19 12:06:29 +0900

    chore(NavBar.tsx): 쓸데없는 props제거

--- src/components/@common/@Layout/Layout/_fragments/NavBar.tsx
+++ src/components/@common/@Layout/Layout/_fragments/NavBar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import { ListItem, Text, UnorderedList, ListProps } from '@chakra-ui/react';
+import { ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import { NAVBAR_DATA } from '@constants/navBarData';
 
-const NavBar = ({ ...css }: ListProps) => {
+const NavBar = () => {
   return (
-    <UnorderedList listStyleType="none" m="0" {...css}>
+    <UnorderedList listStyleType="none" m="0">
       {NAVBAR_DATA.map(({ linkName, link }, idx, allArr) => {
         const isLastIdx = idx === allArr.length - 1;
         return (
