d2e6d3273e556278dbdb5679bf527d516031e229
안병진 <byungjin0120@gmail.com>
2023-02-20 22:46:13 +0900

    chore: 링크를 임시방편으로 다 홈으로 연결

--- src/constants/navBarData.ts
+++ src/constants/navBarData.ts
@@ -1,6 +1,6 @@
 export const NAVBAR_DATA: { link: string; linkName: string }[] = [
-  { link: '', linkName: 'Blog' },
-  { link: '', linkName: 'Snippets' },
-  { link: '', linkName: 'Search' },
-  { link: '', linkName: 'About' },
+  { link: '/', linkName: 'Blog' },
+  { link: '/', linkName: 'Snippets' },
+  { link: '/', linkName: 'Search' },
+  { link: '/', linkName: 'About' },
 ];
