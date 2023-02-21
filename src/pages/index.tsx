ca83bb149b838b0de89612b89684c0c21a052cb3
BByungs <byungjin0120@gmail.com>
2023-02-21 19:52:39 +0900

    chore(Home): 블로그리스트 staticProps로 받아옴

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import Layout from '@components/@common/@Layout/Layout';
 import HomePage from '@components/HomePage/HomePage';
-import withHead from '@hocs/withHead';
 
-const Home = () => <Layout p="0px 20px" mt="20px" content={<HomePage />} />;
+import { allBlogs } from 'contentlayer/generated';
+import { InferGetStaticPropsType, NextPage } from 'next';
 
-export default withHead(Home);
+export const getStaticProps = async () => {
+  const blogs = allBlogs;
+
+  return {
+    props: {
+      blogs,
+    },
+  };
+};
+
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  blogs,
+}) => {
+  return <Layout p="0px 20px" mt="20px" content={<HomePage blogs={blogs} />} />;
+};
+
+export default Home;
