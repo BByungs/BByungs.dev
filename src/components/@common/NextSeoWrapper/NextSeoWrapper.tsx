f19686d7e8d6820ecf09d8e6ad37136cc40d6eae
안병진 <byungjin0120@gmail.com>
2023-02-21 23:22:32 +0900

    chore(NextSeoWrapper): 이미지 타입 부여

--- src/components/@common/NextSeoWrapper/NextSeoWrapper.tsx
+++ src/components/@common/NextSeoWrapper/NextSeoWrapper.tsx
@@ -14,6 +14,8 @@ const NextSeoWrapper = ({ metaData, children }: NextSeoWrapperProps) => {
     return metaData.url === '/';
   }, [metaData.url]);
   const { colorMode } = useColorMode();
+  // 이미지의 이름에 .이 포함되면 안된다
+  const [_, ext] = metaData.thumbnailUrl.split('.');
 
   return (
     <React.Fragment>
@@ -27,6 +29,7 @@ const NextSeoWrapper = ({ metaData, children }: NextSeoWrapperProps) => {
         <meta property="og:image" content={metaData.thumbnailUrl} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content={`image/${ext}`} />
       </Head>
 
       <React.Fragment>
