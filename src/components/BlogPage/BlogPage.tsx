53d57f124c3ac4817db41df0fbe52be1b5ff5424
BByungs <byungjin0120@gmail.com>
2023-02-21 20:41:36 +0900

    chore(BlogPage): thumbnailUrl 추가

--- src/components/BlogPage/BlogPage.tsx
+++ src/components/BlogPage/BlogPage.tsx
@@ -14,6 +14,7 @@ const BlogPage = ({ post }: BlogLayoutProps) => {
     description: post.description,
     date: post.date,
     url: post.slug,
+    thumbnailUrl: post.thumbnailUrl,
   };
 
   const Component = useMDXComponent(post.body.code);
