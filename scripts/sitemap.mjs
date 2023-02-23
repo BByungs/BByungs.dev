6e312e9c595063b7d343d56ca7ed333aaba12b43
BByungs <byungjin0120@gmail.com>
2023-02-23 12:30:01 +0900

    chore(sitemap.mjs): sitemap에 홈페이지 추가

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -7,6 +7,9 @@ const pages = await globby(['pages/*.tsx', 'posts/**/*.mdx', '!pages/_*.tsx']);
 const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${`https://devahn.vercel.app`}</loc>
+  </url>
     ${pages
       .map((page) => {
         const path = page
