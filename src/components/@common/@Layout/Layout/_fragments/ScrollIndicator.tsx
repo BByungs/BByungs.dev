59dbde4338641792b552b171545f6240405c1000
안병진 <byungjin0120@gmail.com>
2023-02-24 15:13:55 +0900

    chore(ScrollIndicator): scroll indicator가 들어가는 페이지는 스크롤바 제거

--- src/components/@common/@Layout/Layout/_fragments/ScrollIndicator.tsx
+++ src/components/@common/@Layout/Layout/_fragments/ScrollIndicator.tsx
@@ -4,14 +4,25 @@ import useScrollIndicator from '@hooks/useScrollIndicator';
 const ScrollIndicator = () => {
   const { scrollRatio } = useScrollIndicator();
   return (
-    <Box
-      h="66px"
-      bgColor="transparent"
-      borderBottomWidth="6px"
-      borderColor="teal.500"
-      w={`${scrollRatio}%`}
-      pos="absolute"
-    />
+    <>
+      <Box
+        h="66px"
+        bgColor="transparent"
+        borderBottomWidth="6px"
+        borderColor="teal.500"
+        w={`${scrollRatio}%`}
+        pos="absolute"
+      />
+      <style global jsx>{`
+        body {
+          -ms-overflow-style: none; /* IE and Edge */
+          scrollbar-width: none; /* Firefox */
+        }
+        body::-webkit-scrollbar {
+          display: none; /* Chrome, Safari, Opera*/
+        }
+      `}</style>
+    </>
   );
 };
 
