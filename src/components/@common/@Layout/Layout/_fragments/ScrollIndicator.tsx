4a4c15e45587c51835341b9f836c7c689c398d6d
안병진 <byungjin0120@gmail.com>
2023-02-24 12:13:38 +0900

    feat(ScrollIndicator): 헤더에 스크롤이 얼마나 내려갔는지를 보여주는 컴포넌트 추가

---
+++ src/components/@common/@Layout/Layout/_fragments/ScrollIndicator.tsx
@@ -0,0 +1,18 @@
+import { Box, BoxProps } from '@chakra-ui/react';
+import useScrollIndicator from '@hooks/useScrollIndicator';
+
+const ScrollIndicator = () => {
+  const { scrollRatio } = useScrollIndicator();
+  return (
+    <Box
+      h="66px"
+      bgColor="transparent"
+      borderBottomWidth="5px"
+      borderColor="teal.500"
+      w={`${scrollRatio}%`}
+      pos="absolute"
+    />
+  );
+};
+
+export default ScrollIndicator;
