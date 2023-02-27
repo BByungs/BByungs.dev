76902695026be368057f9eaf9596a7c1b3ad1716
BByungs <byungjin0120@gmail.com>
2023-02-27 12:24:29 +0900

    chore(contentlayer.config.ts): Snippets 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -34,7 +34,41 @@ export const Blog = defineDocumentType(() => ({
   },
 }));
 
+export const Snippets = defineDocumentType(() => ({
+  name: 'Snippets',
+  filePathPattern: `snippets/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    thumbnailUrl: {
+      type: 'string',
+      required: true,
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string' },
+    },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
+    },
+  },
+}));
+
 export default makeSource({
   contentDirPath: 'posts', // mdx파일들이 몰려있는 폴더
-  documentTypes: [Blog],
+  documentTypes: [Blog, Snippets],
 });
