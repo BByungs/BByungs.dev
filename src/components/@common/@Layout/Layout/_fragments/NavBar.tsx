d30fee2d4a44f2637d11141002df517b8fb811ef
BByungs <byungjin0120@gmail.com>
2023-02-27 15:23:09 +0900

    chore(NavBar): 새로 추가한 NavItem 컴포넌트 적용

--- src/components/@common/@Layout/Layout/_fragments/NavBar.tsx
+++ src/components/@common/@Layout/Layout/_fragments/NavBar.tsx
@@ -1,38 +1,20 @@
 import React from 'react';
-import Link from 'next/link';
-import { ListItem, Text, UnorderedList, useColorMode } from '@chakra-ui/react';
+import { UnorderedList } from '@chakra-ui/react';
 import { NAVBAR_DATA } from '@constants/navBarData';
-import { useRouter } from 'next/router';
+import NavItem from './NavItem';
 
 const NavBar = () => {
-  const { pathname } = useRouter();
-  const { colorMode } = useColorMode();
-
   return (
     <UnorderedList listStyleType="none" m="0">
       {NAVBAR_DATA.map(({ linkName, link }, idx, allArr) => {
         const isLastIdx = idx === allArr.length - 1;
         return (
-          <ListItem
-            float="left"
+          <NavItem
             key={`NAVBAR_${linkName}`}
+            linkName={linkName}
+            link={link}
             mr={isLastIdx ? '0px' : '20px'}
-          >
-            <Link href={link}>
-              <Text
-                fontWeight="bold"
-                color={
-                  pathname === link
-                    ? 'teal.500'
-                    : colorMode === 'light'
-                    ? 'black'
-                    : 'white'
-                }
-              >
-                {linkName}
-              </Text>
-            </Link>
-          </ListItem>
+          />
         );
       })}
     </UnorderedList>
