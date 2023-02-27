2dcb076d8dd893056d2533b0c66211de0811b69b
BByungs <byungjin0120@gmail.com>
2023-02-27 15:42:18 +0900

    chore(NavBar): 모바일에서 useNavBarColor가 에러나서 다시 원래대로 롤백

--- src/components/@common/@Layout/Layout/_fragments/NavBar.tsx
+++ src/components/@common/@Layout/Layout/_fragments/NavBar.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import { UnorderedList } from '@chakra-ui/react';
+import Link from 'next/link';
+import { ListItem, Text, UnorderedList, useColorMode } from '@chakra-ui/react';
 import { NAVBAR_DATA } from '@constants/navBarData';
-import NavItem from './NavItem';
+import { useRouter } from 'next/router';
 
 const NavBar = () => {
+  const { pathname } = useRouter();
+  const { colorMode } = useColorMode();
+
   return (
     <UnorderedList listStyleType="none" m="0">
       {NAVBAR_DATA.map(({ linkName, link }, idx, allArr) => {
         const isLastIdx = idx === allArr.length - 1;
         return (
-          <NavItem
+          <ListItem
+            float="left"
             key={`NAVBAR_${linkName}`}
-            linkName={linkName}
-            link={link}
             mr={isLastIdx ? '0px' : '20px'}
-          />
+          >
+            <Link href={link}>
+              <Text
+                fontWeight="bold"
+                color={
+                  pathname === link
+                    ? 'teal.500'
+                    : colorMode === 'light'
+                    ? 'black'
+                    : 'white'
+                }
+              >
+                {linkName}
+              </Text>
+            </Link>
+          </ListItem>
         );
       })}
     </UnorderedList>
