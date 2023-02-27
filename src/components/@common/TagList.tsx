6f0ca51d7cacdab9e313dad2b8cca37dcd07b260
BByungs <byungjin0120@gmail.com>
2023-02-27 18:44:05 +0900

    chore(TagList): TagList 공통컴포넌트로 뺐음

--- src/components/@common/TagList.tsx
+++ src/components/@common/TagList.tsx
@@ -1,15 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, FlexProps } from '@chakra-ui/react';
 import React from 'react';
 import TagButton from './TagButton';
 
-interface TagListProps {
+interface TagListProps extends FlexProps {
   tagList: string[];
   selectTag: string;
   handleTag: (tag: string) => void;
 }
-const TagList = ({ tagList, handleTag, selectTag }: TagListProps) => {
+const TagList = ({ tagList, handleTag, selectTag, ...css }: TagListProps) => {
   return (
-    <Flex gap="10px" flexWrap="wrap" mb="40px">
+    <Flex gap="10px" flexWrap="wrap" {...css}>
       {tagList.map((tag) => (
         <TagButton
           key={tag}
