5258dc18614e3ab1dcc88bbcc480c0d87fde5470
BByungs <byungjin0120@gmail.com>
2023-02-28 16:41:12 +0900

    chore(ColorModeButton): 쓸데없는 bg제거

--- src/components/@common/@Layout/Layout/_fragments/ColorModeButton.tsx
+++ src/components/@common/@Layout/Layout/_fragments/ColorModeButton.tsx
@@ -8,13 +8,11 @@ const ColorModeButton = () => {
     <SunIcon boxSize="24px" />,
     <MoonIcon boxSize="24px" />
   );
-  const buttonBg = useColorModeValue('white', 'gray.800');
   return (
     <Button
       onClick={toggleColorMode}
       variant="unstyled"
       boxSize="24px"
-      bg={buttonBg}
       borderRadius="100%"
       aria-label="color-mode-btn"
     >
