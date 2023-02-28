21249ba048533626a3eb5e95224339b24bb3bed8
BByungs <byungjin0120@gmail.com>
2023-02-28 18:56:06 +0900

    chore(SearchButton): SearchModal 적용

--- src/components/@common/@Layout/Layout/_fragments/SearchButton.tsx
+++ src/components/@common/@Layout/Layout/_fragments/SearchButton.tsx
@@ -1,33 +1,68 @@
-import React from 'react';
-import { Button, Flex, Icon, Text } from '@chakra-ui/react';
+import React, { useCallback, useEffect } from 'react';
+import { Button, Flex, Icon, Text, useDisclosure } from '@chakra-ui/react';
 import { FiCommand } from 'react-icons/fi';
+import SearchModal from './SearchModal';
 
 const SearchButton = () => {
-  return (
-    <Button
-      type="button"
-      variant="unstyled"
-      p="10px"
-      h="35px"
-      bgColor="#eaeaea"
-      borderRadius="10px"
-      display="flex"
-      columnGap="8px"
-    >
-      <Text color="black">Search</Text>
+  const {
+    isOpen: isSearchOpen,
+    onOpen: openSearch,
+    onClose: closeSearch,
+  } = useDisclosure();
 
-      <Flex
-        borderRadius="5px"
-        bgColor="white"
-        columnGap="3px"
-        alignItems="center"
-        py="2px"
-        px="5px"
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      // todo window일땐 시작버튼 , mac일땐 command+k로 해야 함
+      /**
+       * if (window) {
+       *  window key 감지 했을때 openSearch()
+       * } else {
+       *  command+k 감지 했을때 openSearch
+       * }
+       */
+
+      // Command + K 버튼을 눌렀을때
+      if (event.metaKey && event.key === 'k') {
+        isSearchOpen ? closeSearch() : openSearch();
+      }
+    },
+    [closeSearch, isSearchOpen, openSearch]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, [handleKeyPress]);
+
+  return (
+    <React.Fragment>
+      <Button
+        type="button"
+        variant="unstyled"
+        p="10px"
+        h="35px"
+        bgColor="#eaeaea"
+        borderRadius="10px"
+        display="flex"
+        columnGap="8px"
+        onClick={openSearch}
       >
-        <Icon as={FiCommand} color="black" boxSize="12px" />
-        <Text color="black">K</Text>
-      </Flex>
-    </Button>
+        <Text color="black">Search</Text>
+        <Flex
+          borderRadius="5px"
+          bgColor="white"
+          columnGap="3px"
+          alignItems="center"
+          py="2px"
+          px="5px"
+        >
+          <Icon as={FiCommand} color="black" boxSize="12px" />
+          <Text color="black">K</Text>
+        </Flex>
+      </Button>
+
+      <SearchModal isOpen={isSearchOpen} onClose={closeSearch} />
+    </React.Fragment>
   );
 };
 
