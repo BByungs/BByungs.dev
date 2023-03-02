2ab87549ba8705f71c0a93cfd0dfc8a6fae4e85e
안병진 <byungjin0120@gmail.com>
2023-03-02 22:39:38 +0900

    chore: minify안하면 에러남 ^_^...

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { withContentlayer } = require('next-contentlayer');
 
 module.exports = withContentlayer({
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['github.com'],
   },
