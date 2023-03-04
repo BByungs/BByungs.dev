cb36359c307de5ee7368dda9e350d3ba487d1d7c
안병진 <byungjin0120@gmail.com>
2023-03-04 11:21:38 +0900

    chore(UserImage.tsx): objectFit를 그냥 적용하는방법은 deprecated 되어 알맞는 방법 사용

--- src/components/HomePage/_fragments/UserImage.tsx
+++ src/components/HomePage/_fragments/UserImage.tsx
@@ -26,7 +26,13 @@ import { Box } from '@chakra-ui/react';
 const UserImage = ({ src }: Pick<ImageProps, 'src'>) => {
   return (
     <Box borderRadius="100%" overflow="hidden" boxSize="90px" pos="relative">
-      <Image fill priority src={src} alt="user-photo" objectFit="cover" />
+      <Image
+        fill
+        priority
+        src={src}
+        alt="user-photo"
+        style={{ objectFit: 'cover' }}
+      />
     </Box>
   );
 };
