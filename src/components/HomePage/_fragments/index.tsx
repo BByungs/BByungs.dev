8437eb6eaf1f46505dd59bcc46abd11ba09ef4bd
안병진 <byungjin0120@gmail.com>
2023-03-06 20:28:56 +0900

    chore: 필요없는 파일 삭제

--- src/components/HomePage/_fragments/index.tsx
+++ src/components/HomePage/_fragments/index.tsx
@@ -1,6 +1,5 @@
 import UserInfo from './UserInfo';
-import UserImage from './UserImage';
 import ContactLink from './ContactLink';
 import ContactLinkList from './ContactLinkList';
 
-export { UserInfo, UserImage, ContactLink, ContactLinkList };
+export { UserInfo, ContactLink, ContactLinkList };
