ce9679bd46f217a67c35cc39b795c625c40a5286
안병진 <byungjin0120@gmail.com>
2023-03-07 22:08:22 +0900

    feat(PostOrganism.tsx): PostOrganism 컴포넌트 추가

---
+++ src/components/@common/PostOrganism.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import PostList from './PostList';
+import TagList from './TagList';
+import { DocumentTypes } from 'contentlayer/generated';
+import { Box, Text } from '@chakra-ui/react';
+import { maxWidth } from '@constants/maxWidth';
+
+interface PostOrganismProps<T> {
+  posts: T[];
+}
+
+const PostOrganism = <T extends DocumentTypes>({
+  posts,
+}: PostOrganismProps<T>) => {
+  const existPosts = !!posts.length;
+  const tagList = existPosts
+    ? ['ALL'].concat(
+        Array.from(new Set(posts.flatMap(({ tags }) => tags ?? ''))).filter(
+          (tag) => !!tag.length,
+        ),
+      )
+    : [];
+  const [selectTag, setSelectTag] = useState<string>('ALL');
+  const handleTag = (tag: string) => setSelectTag(tag);
+
+  return existPosts ? (
+    <React.Fragment>
+      {/* Tag List */}
+      <TagList
+        tagList={tagList}
+        handleTag={handleTag}
+        selectTag={selectTag}
+        mb="40px"
+      />
+      {/* Blog List */}
+      <PostList posts={posts} selectTag={selectTag} />
+    </React.Fragment>
+  ) : (
+    <Box pos="absolute" top="50%" w={maxWidth}>
+      <Text textAlign="center" textStyle="md_bold">
+        No Posts!!
+      </Text>
+    </Box>
+  );
+};
+
+export default PostOrganism;
