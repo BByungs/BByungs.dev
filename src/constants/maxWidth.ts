6a1be9a3a30640c493997c34f1ad0abd6ed96ddc
안병진 <byungjin0120@gmail.com>
2023-03-07 23:54:33 +0900

    feat(maxWidth.ts): ommitedPaddingMaxWidth 추가

--- src/constants/maxWidth.ts
+++ src/constants/maxWidth.ts
@@ -2,3 +2,8 @@ export const maxWidth = {
   base: '100%',
   sm: '768px',
 };
+
+export const ommitedPaddingMaxWidth = {
+  base: `calc(${maxWidth.base} - 40px)`,
+  sm: `calc(${maxWidth.sm} - 40px)`,
+};
