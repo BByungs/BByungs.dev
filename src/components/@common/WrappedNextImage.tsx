a226f7a86c463df8b384ab563facd25b329d945d
안병진 <byungjin0120@gmail.com>
2023-03-07 20:52:26 +0900

    chore(WrappedNextImage): 쓸데없는 주석 제거 및 타입 수정

--- src/components/@common/WrappedNextImage.tsx
+++ src/components/@common/WrappedNextImage.tsx
@@ -1,15 +1,13 @@
 import Image, { ImageProps } from 'next/image';
-
 import { Box, BoxProps } from '@chakra-ui/react';
 
-type BoxOmitted = 'w' | 'width' | 'h' | 'height';
-type NextImageOmitted = 'src' | 'alt';
-interface WrappedNextImageProps extends Omit<BoxProps, BoxOmitted> {
+type Omitted = 'src' | 'alt';
+export type WrappedNextImageType = Omit<ImageProps, Omitted>;
+
+interface WrappedNextImageProps extends BoxProps {
   src: string;
   alt: string;
-  width: string;
-  height: string;
-  nextImageProps?: Omit<ImageProps, NextImageOmitted>;
+  nextImageProps?: WrappedNextImageType;
 }
 /**
  * @param width Image의 가로 크기를 넣으시면 됩니다.
@@ -23,16 +21,14 @@ interface WrappedNextImageProps extends Omit<BoxProps, BoxOmitted> {
 const WrappedNextImage = ({
   src,
   alt,
-  width,
-  height,
   nextImageProps,
   ...props
 }: WrappedNextImageProps) => {
   return (
     <Box
       pos={props.pos || props.position || 'relative'}
-      w={width}
-      h={height}
+      w={props.width || props.w || '100%'}
+      h={props.height || props.h || '150px'}
       {...props}
     >
       <Image src={src} alt={alt} fill {...nextImageProps} />
@@ -41,24 +37,3 @@ const WrappedNextImage = ({
 };
 
 export default WrappedNextImage;
-
-/**
- * @Description priority에 대해서
- * When true, the image will be considered high priority and preload.
- * Should only be used when the image is visible above the fold.
- * Defaults to false.
- *
- * true인 경우 이미지가 높은 우선 순위와 preload로 간주된다.
- * 이미지가 웹 사이트 상단부에 보여져야하는 경우에만 사용해야 한다.
- * (priority의 기본값은 false이다.)
- */
-
-/**
- * @Description Next Image를 사용할때 상위 요소에 맞추는 방법:
- * Next Image에 fill옵션을 true로 주면 부모요소의 크기를 따라간다.
- * 단, 상위요소에는 position이 absolute나 relative가 걸려있어야 한다.
- * 그렇지 않으면 이미지가 전체화면에 다 덮여진다.
- *
- * Next Image에 fill옵션을 적용하면 width, height를 사용하지 못한다.
- * 쓸거면 둘 중 하나는 포기해야함.
- */
