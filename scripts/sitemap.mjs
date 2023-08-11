dc7cae32b2618300b0af2eed0821585ab859b024
BByungs <byungjin0120@gmail.com>
2023-08-11 18:08:55 +0900

    chore : sitemap.mjs - add attribute urlset

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -6,7 +6,11 @@ const pages = await globby(['pages/*.tsx', 'posts/**/*.mdx', '!pages/_*.tsx']);
 
 const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
     <loc>${`https://devahn.vercel.app`}</loc>
   </url>
