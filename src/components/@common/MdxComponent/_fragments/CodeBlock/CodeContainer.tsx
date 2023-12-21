30be94dcbfc83e521068c7e1b50d1e527747c900
BByungs <byungjin0120@gmail.com>
2023-12-21 18:36:52 +0900

    feat: code block 스타일 my 수정

--- src/components/@common/MdxComponent/_fragments/CodeBlock/CodeContainer.tsx
+++ src/components/@common/MdxComponent/_fragments/CodeBlock/CodeContainer.tsx
@@ -4,7 +4,7 @@ function CodeContainer(props: BoxProps) {
   return (
     <Box
       rounded="8px"
-      my="8"
+      my="10px"
       bg="#011627"
       sx={{ '& > div': { paddingBlock: '5', paddingEnd: '4' } }}
       {...props}
